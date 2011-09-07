e677040d1063db32b8f97d9f846ce38a56ad0514
= <=>
2011-09-07 14:42:27 -0400

    added dependent destroy

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,7 +2,7 @@ class Country < ActiveRecord::Base
   validates_presence_of :currency, :country
   validates_length_of :currency, :in => 3..3, :message => "Must enter a valid 3 digit currency code"
   has_many :providers, :dependent => :destroy
-  has_many :users
+  has_many :users, :dependent => :destroy
   
   def update_conversion_rate
     url = "http://webservicex.net/CurrencyConvertor.asmx/ConversionRate?FromCurrency=#{currency.upcase}&ToCurrency=USD"
